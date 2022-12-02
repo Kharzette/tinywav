1c175cb8a47892dcc9df3b163c110b6663b9e880
Kharzette <6589537+Kharzette@users.noreply.github.com>
2022-12-02 05:20:31 -0600

    Had an odd segfault with the 2D array.  Didn't make any sense really but I changed it to pointer offset and it works
    Added a 16 bit read that doesn't expand into a float

--- tinywav.c
+++ tinywav.c
@@ -226,8 +226,9 @@ int tinywav_read_f(TinyWav *tw, void *data, int len) {
         }
         case TW_SPLIT: { // channel buffer is split e.g. [[LLLL],[RRRR]]
           for (int i = 0, pos = 0; i < tw->numChannels; i++) {
+            int ofs =i * frames_read;
             for (int j = 0; j < frames_read; j++, ++pos) {
-              ((float **) data)[i][j] = (float) interleaved_data[j*tw->numChannels + i] / INT16_MAX;
+              ((float *) data)[ofs + j] = (float) interleaved_data[j*tw->numChannels + i] / INT16_MAX;
             }
           }
           return frames_read;
@@ -266,6 +267,73 @@ int tinywav_read_f(TinyWav *tw, void *data, int len) {
     }
     default: return 0;
   }
+}
+
+int tinywav_read_16(TinyWav *tw, void *data, int len) {
+  switch (tw->sampFmt) {
+    case TW_INT16: {
+      int16_t *interleaved_data = (int16_t *) alloca(tw->numChannels*len*sizeof(int16_t));
+      size_t samples_read = fread(interleaved_data, sizeof(int16_t), tw->numChannels*len, tw->f);
+      int valid_len = (int) samples_read / tw->numChannels;
+      switch (tw->chanFmt) {
+        case TW_INTERLEAVED: { // channel buffer is interleaved e.g. [LRLRLRLR]
+          for (int pos = 0; pos < tw->numChannels * valid_len; pos++) {
+            ((int16_t *) data)[pos] = interleaved_data[pos];
+          }
+          return valid_len;
+        }
+        case TW_INLINE: { // channel buffer is inlined e.g. [LLLLRRRR]
+          for (int i = 0, pos = 0; i < tw->numChannels; i++) {
+            for (int j = i; j < valid_len * tw->numChannels; j += tw->numChannels, ++pos) {
+              ((int16_t *) data)[pos] = interleaved_data[j];
+            }
+          }
+          return valid_len;
+        }
+        case TW_SPLIT: { // channel buffer is split e.g. [[LLLL],[RRRR]]
+          for (int i = 0, pos = 0; i < tw->numChannels; i++) {
+            int ofs =i * valid_len;
+            for (int j = 0; j < valid_len; j++, ++pos) {
+              ((int16_t *) data)[ofs + j] = interleaved_data[j*tw->numChannels + i];
+            }
+          }
+          return valid_len;
+        }
+        default: return 0;
+      }
+    }
+    case TW_FLOAT32: {
+      float *interleaved_data = (float *) alloca(tw->numChannels*len*sizeof(float));
+      size_t samples_read = fread(interleaved_data, sizeof(float), tw->numChannels*len, tw->f);
+      int valid_len = (int) samples_read / tw->numChannels;
+      switch (tw->chanFmt) {
+        case TW_INTERLEAVED: { // channel buffer is interleaved e.g. [LRLRLRLR]
+          memcpy(data, interleaved_data, tw->numChannels*valid_len*sizeof(float));
+          return valid_len;
+        }
+        case TW_INLINE: { // channel buffer is inlined e.g. [LLLLRRRR]
+          for (int i = 0, pos = 0; i < tw->numChannels; i++) {
+            for (int j = i; j < valid_len * tw->numChannels; j += tw->numChannels, ++pos) {
+              ((int16_t *) data)[pos] = interleaved_data[j] * INT16_MAX;
+            }
+          }
+          return valid_len;
+        }
+        case TW_SPLIT: { // channel buffer is split e.g. [[LLLL],[RRRR]]
+          for (int i = 0, pos = 0; i < tw->numChannels; i++) {
+            for (int j = 0; j < valid_len; j++, ++pos) {
+              ((int16_t **) data)[i][j] = interleaved_data[j*tw->numChannels + i] * INT16_MAX;
+            }
+          }
+          return valid_len;
+        }
+        default: return 0;
+      }
+    }
+    default: return 0;
+  }
+
+  return len;
 }
 
 void tinywav_close_read(TinyWav *tw) {
